8891fc45867ff87306ee5062668232e450720a78
Sid Prabhakaran <s2siddhu@gmail.com>
2022-07-29 20:04:45 -0700

    more flexible async implementation with multi-thread support

--- include/async.hpp
+++ include/async.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 #include "main.h"
+
 #define Async(code) src::Actions::doAsync([]() code);
 
+static constexpr uint32_t NUM_WORKERS = 3;
+
 namespace src::Actions {
-void doAsync(std::function<void()> func);
+void workerExec(void*);
+
+static const pros::Task asyncWorker1(workerExec, nullptr, "asyncWorker1");
+static const pros::Task asyncWorker2(workerExec, nullptr, "asyncWorker2");
+static const pros::Task asyncWorker3(workerExec, nullptr, "asyncWorker3");
 }  // namespace src::Actions
