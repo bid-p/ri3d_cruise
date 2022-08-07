aa10351d7312a9ac1efcd22e25cc16d71fb681a5
Sid Prabhakaran <s2siddhu@gmail.com>
2022-08-07 15:55:41 -0700

    formatting changes

--- src/chassis/chassis.cpp
+++ src/chassis/chassis.cpp
@@ -13,20 +13,20 @@ std::shared_ptr<ChassisController> chassis =
         .withMotors(left_chassis_group, right_chassis_group)
         .withDimensions(
             AbstractMotor::gearset::blue,
-            {{4_in, 8_in}, imev5BlueTPR}) // {Wheelbase Width, Length}
+            {{4_in, 8_in}, imev5BlueTPR})  // {Wheelbase Width, Length}
         .build();
 
 void initialize() {
-  left_chassis_group.setBrakeMode(AbstractMotor::brakeMode::brake);
-  right_chassis_group.setBrakeMode(AbstractMotor::brakeMode::brake);
+    left_chassis_group.setBrakeMode(AbstractMotor::brakeMode::brake);
+    right_chassis_group.setBrakeMode(AbstractMotor::brakeMode::brake);
 }
 
 void update() {}
 
 void act() {
-  Chassis::chassis->getModel()->arcade(
-      controller.getAnalog(ControllerAnalog::leftY),
-      TURN_LIMITING * controller.getAnalog(ControllerAnalog::rightX));
+    Chassis::chassis->getModel()->arcade(
+        controller.getAnalog(ControllerAnalog::leftY),
+        TURN_LIMITING * controller.getAnalog(ControllerAnalog::rightX));
 }
 
-} // namespace src::Chassis
+}  // namespace src::Chassis
