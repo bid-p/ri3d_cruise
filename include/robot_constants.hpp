09ffd3ed0f5da02f1959f3e4ce88083d67035036
Sharwin Patil <sharwin24@gmail.com>
2022-08-08 18:44:07 -0700

    Reorganized constants

--- include/robot_constants.hpp
+++ include/robot_constants.hpp
@@ -10,23 +10,22 @@ static constexpr uint8_t CHASSIS_PORT_R1 = 4;
 static constexpr uint8_t CHASSIS_PORT_R2 = 7;
 static constexpr uint8_t CHASSIS_PORT_R3 = 3;
 
-static constexpr float TURN_FACTOR = 0.65f;
-
 static constexpr uint8_t FLYWHEEL_PORT = 2;
 static constexpr uint8_t INTAKE_PORT = 1;
 
 // Sensor Ports
 static constexpr uint8_t GATE_PORT = 'A';
-static constexpr uint8_t LEFT_ENCODER_PORT_A = 'B';
-static constexpr uint8_t LEFT_ENCODER_PORT_B = 'C';
-static constexpr uint8_t RIGHT_ENCODER_PORT_A = 'D';
-static constexpr uint8_t RIGHT_ENCODER_PORT_B = 'E';
-static constexpr uint8_t BACK_ENCODER_PORT_A = 'F';
-static constexpr uint8_t BACK_ENCODER_PORT_B = 'G';
-static constexpr uint8_t IMU_PORT = 10;
+static constexpr uint8_t LEFT_ENCODER_PORT_A = 'B';  // Not wired yet
+static constexpr uint8_t LEFT_ENCODER_PORT_B = 'C';  // Not wired yet
+static constexpr uint8_t RIGHT_ENCODER_PORT_A = 'D'; // Not wired yet
+static constexpr uint8_t RIGHT_ENCODER_PORT_B = 'E'; // Not wired yet
+static constexpr uint8_t BACK_ENCODER_PORT_A = 'F';  // Not wired yet
+static constexpr uint8_t BACK_ENCODER_PORT_B = 'G';  // Not wired yet
+static constexpr uint8_t IMU_PORT = 10;              // Not wired yet
 
 // Physical robot constants
 static constexpr float_t WHEEL_DIAMETER = 4.125f;        // [in]
 static constexpr float_t CENTER_TO_LEFT_ENCODER = 0.0f;  // [in]
 static constexpr float_t CENTER_TO_RIGHT_ENCODER = 0.0f; // [in]
 static constexpr float_t CENTER_TO_BACK_ENCODER = 0.0f;  // [in]
+static constexpr float TURN_FACTOR = 0.65f;
