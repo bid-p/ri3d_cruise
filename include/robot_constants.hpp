28d5e833ec2326497dac4e690b496fe1785b0754
Sharwin Patil <sharwin24@gmail.com>
2022-08-08 18:41:12 -0700

    Merge changes

--- include/robot_constants.hpp
+++ include/robot_constants.hpp
@@ -18,8 +18,6 @@ static constexpr float TURN_FACTOR = 0.65f;
 static constexpr uint8_t FLYWHEEL_PORT = 2;
 static constexpr uint8_t INTAKE_PORT = 1;
 
-static constexpr uint8_t GATE_PORT = 'A';
-
 // Sensor Ports
 static constexpr uint8_t GATE_PORT = 'A';
 static constexpr uint8_t LEFT_ENCODER_PORT_A = 'B';
@@ -31,7 +29,7 @@ static constexpr uint8_t BACK_ENCODER_PORT_B = 'G';
 static constexpr uint8_t IMU_PORT = 10;
 
 // Physical robot constants
-static constexpr float_t WHEEL_DIAMETER = 4.125f;         // [in]
-static constexpr float_t CENTER_TO_LEFT_ENCODER = 0.0f;   // [in]
-static constexpr float_t CENTER_TO_RIGHT_ENCODER = 0.0f;  // [in]
-static constexpr float_t CENTER_TO_BACK_ENCODER = 0.0f;   // [in]
+static constexpr float_t WHEEL_DIAMETER = 4.125f;        // [in]
+static constexpr float_t CENTER_TO_LEFT_ENCODER = 0.0f;  // [in]
+static constexpr float_t CENTER_TO_RIGHT_ENCODER = 0.0f; // [in]
+static constexpr float_t CENTER_TO_BACK_ENCODER = 0.0f;  // [in]
